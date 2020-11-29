dc8ec3741f80e0a34cb5f8f770fc5b818143e7de
Pierrejean BESNARD <pj.besnard@outlook.com>
2020-11-29 01:31:18 +0100

    inCart function added

--- src/fr/uge/webservices/Service.java
+++ src/fr/uge/webservices/Service.java
@@ -24,7 +24,7 @@ public class Service {
 	private final ICarDataBase cars; 
 	private final BankService bank;
 	
-	private final Set<Long> basket = new HashSet<>();
+	private final Set<Long> cart = new HashSet<>();
 
 	public Service() throws RemoteException, ServiceException, MalformedURLException, NotBoundException {
 		cars = (ICarDataBase) Naming.lookup("rmi://localhost:1099/CarDataBase");
@@ -71,19 +71,14 @@ public class Service {
 	
 	public boolean buyCar(long carId, String login, String password) throws RemoteException, ServiceException {
 		var car = cars.getCar(carId);
-		
 		if (car == null || !bank.isAnAccount(login, password) || !car.isSellable()) return false;
-		
-		
-		
 		var p = car.getSellPrice();
-
 		if(bank.amountAvailable(login, password, p)) {
 			bank.send(login, password, p);
 			cars.removeCar(carId);
 			removeBasket(carId);
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 		
@@ -95,29 +90,33 @@ public class Service {
 		return c.activeCurrencies("");
 	}
 	
-	public boolean addBasket(long carId) throws RemoteException {
+	public boolean addCart(long carId) throws RemoteException {
 		var car = cars.getCar(carId);
 		if (car == null || !car.isSellable()) return false;
 		
-		return basket.add(carId);
+		return cart.add(carId);
 	}
 	
 	public boolean removeBasket(long carId) {
-		return basket.remove(carId);
+		return cart.remove(carId);
 	}
 	
 	public boolean isInBasket(long carId) {
-		return basket.contains(carId);
+		return cart.contains(carId);
 	}
 	
-	public String basketToJson() throws RemoteException {
+	public String cartToJson() throws RemoteException {
 		var sj = new StringJoiner(", ", "[", "]");
-		for (Long carId : basket) {
+		for (Long carId : cart) {
 			var car = cars.getCar(carId);
 			sj.add(car.toJson(carId));
 		}
 		
 		return "{" + sj.toString() + "}";
+	}
+	
+	public boolean inCart(long carId) {
+		return cart.contains(carId);
 	}
 	
 
